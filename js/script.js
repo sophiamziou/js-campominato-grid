beb90075b50fa7736ed5985acb899c83c8f496c0
sophiamziou <sofiamziou@gmail.com>
2022-12-10 23:27:00 +0100

    3rd point not working

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-
 // 1- visualizzare l'elemento al click del bottone
-
 const play = document.querySelector('.play');
 
 play.addEventListener('click', function(){
     document.querySelector('.field').style.visibility = "visible";
 })
 
-
 // 2- creare la griglia 10x10 
-
 const container = document.querySelector('.field');
-
 for (i=0; i<100; i++){
     let element;
     element = `<div class="box">${i+1}</div>`;
@@ -19,19 +14,7 @@ for (i=0; i<100; i++){
 }
 
 // 3- cliccando sulle celle si colorano e compare il numero in console
-
-// const cell = document.querySelector('.box');
-
-// cell.addEventListener('click', function(){
-//     cell.style.backgroundColor = "red";  
-// })
-
-// const box = document.getElementsByClassName('box');
-
-// BONUS
-
-
-
-
-
-
+const cell = document.getElementsByClassName("box");
+cell.addEventListener('click', function(){
+    cell.style.backgroundColor = "red";  
+})
